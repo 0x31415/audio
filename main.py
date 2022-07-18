1287ae693427210e83217da7c960c5f4bffd2269
Nico_ <n.lepetit1@gmail.com>
2022-07-18 19:27:52 +0200

    update main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from src.audiosources.sinus import Sinus
+from src.audiosources.square import Square
+
 
 def main():
-    print("Hello World")
+    sin = Sinus(300.0, 44100, 4, 1.0)
+    sq = Square(300.0, 44100, 3, 0.1)
+    sq.save("test.wav")
 
 if __name__ == "__main__":
     main()
